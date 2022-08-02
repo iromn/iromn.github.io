edc618e6a5ae4301c93b8ae7e47346d2cc4ec069
Nganba <90682750+NganbaIrom@users.noreply.github.com>
2022-08-02 14:56:27 +0530

    Initial Commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    <h1>iromn.github.io</h1>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LandingPage',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <LandingPage msg="Coming Soon"/>
+    <img alt="Vue logo" src="../assets/under_dev.jpg">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LandingPage from '@/components/LandingPage.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LandingPage
+  }
+}
+</script>
